986a269b29932bdcffec847320e653b02a895ace
BeroAdel116 <beroma78@gmail.com>
2024-08-08 03:58:03 +0300

    added webhook

--- middleware/deleteFromDB.js
+++ middleware/deleteFromDB.js
@@ -1,8 +1,6 @@
-import cloudinary from "../service/cloudinary.js"
-
 export const deleteFromDB = async(req,res,next)=>{
     if(req?.data){
-        const {model, data} = req.data
+        const {model, id} = req.data
         await model.deleteOne({_id:id})
     }
 }
